4689fb4ca0c7f92f244ba1e6de579fb5f99e2028
Dawit Mebrahtu <dawitaraya7@gmail.com>
2018-09-08 09:25:28 -0500

    First commit. Uploading all files up to assignment 4-3.

---
+++ Assignments/src/lab4_3/employeeinfo/CheckingAccount.java
@@ -0,0 +1,25 @@
+package lab4_3.employeeinfo;
+
+public class CheckingAccount extends Account {
+
+	public CheckingAccount(Employee e, double amount) {
+		super(e, amount);
+	}
+	
+	public AccountType getAcctType() {
+		return AccountType.CHECKING;
+	}
+	
+	@Override
+	public double getBalance() {
+		double baseBalance = super.getBalance();
+		return baseBalance - 5;
+	}
+
+	@Override
+	public String toString() {
+		 return "Account type: " + getAcctType() + "\n Current bal: $" + super.getBalance();
+	}
+	
+	
+}
